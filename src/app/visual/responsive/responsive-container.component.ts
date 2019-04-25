f0fe08177711c6c0f9f9d15899339efc6ec30957
BLACKBAUD\Trevor.Burch <Trevor.Burch@blackbaud.com>
2019-04-24 23:19:19 -0400

    Updated demo to be more like what is intended and reverted change made around inner containers

---
+++ src/app/visual/responsive/responsive-container.component.ts
@@ -0,0 +1,10 @@
+import {
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'sky-responsive-container',
+  templateUrl: './responsive-container.component.html',
+  styleUrls: ['./responsive-container.component.scss']
+})
+export class SkyResponsiveContainerComponent {}
