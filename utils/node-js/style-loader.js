3699106d690b1b186435542b6d9f5bb8675415de
Bobby Earl <bobby.earl@blackbaud.com>
2019-05-31 08:36:05 -0400

    Adjusted JS style loader.

--- utils/node-js/style-loader.js
+++ utils/node-js/style-loader.js
@@ -1,6 +1,10 @@
 /* jshint node: true */
 'use strict';
 
+/**
+ * This file is almost identical to src/app/public/style-loader.ts.
+ * It exists separately as JS in order to be used globally in unit tests.
+ */
 require('style-loader!@skyux/theme/css/sky.css');
 
 const FontFaceObserver = require('fontfaceobserver');
@@ -8,18 +12,17 @@ const LOAD_TIMEOUT = 30000;
 
 module.exports = {
   loadStyles: function () {
-    const fontAwesome4 = new FontFaceObserver('FontAwesome');
-    const fontAwesome5 = new FontFaceObserver('Font Awesome 5 Free');
+    const fontAwesome4 = new FontFaceObserver('FontAwesome', { weight: 900 });
+    const fontAwesome5 = new FontFaceObserver('Font Awesome 5 Brands', { weight: 400 });
     const blackbaudSans = new FontFaceObserver('Blackbaud Sans');
 
     return Promise.all(
       [
-        // Specify a character for FontAwesome since some browsers will fail to detect
-        // when the font is loaded unless a known character with a different width
-        // than the default is not specified.
+        // FA does not contain the latin characters "BESbwy", so we need a custom test string.
+        // https://github.com/bramstein/fontfaceobserver#how-to-use
         fontAwesome4.load('\uf0fc', LOAD_TIMEOUT),
-        fontAwesome5.load('\uf0fc', LOAD_TIMEOUT),
-        blackbaudSans.load(null, LOAD_TIMEOUT)
+        fontAwesome5.load('\uf415', LOAD_TIMEOUT),
+        blackbaudSans.load(undefined, LOAD_TIMEOUT)
       ]
     );
   }
