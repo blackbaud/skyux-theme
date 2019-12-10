999d348f55b7da66c84ad7cd41b32bdae19cbe02
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2019-12-10 12:21:43 -0500

    Status indicator > Add docs

--- src/app/app-extras.module.ts
+++ src/app/app-extras.module.ts
@@ -7,6 +7,11 @@ import {
 } from '@skyux/core';
 
 import {
+  SkyDocsToolsModule,
+  SkyDocsToolsOptions
+} from '@skyux/docs-tools';
+
+import {
   SkyAppLinkModule
 } from '@skyux/router';
 
@@ -17,8 +22,18 @@ import {
 @NgModule({
   exports: [
     SkyAppLinkModule,
+    SkyDocsToolsModule,
     SkyThemeModule,
     SkyMediaQueryModule
+  ],
+  providers: [
+    {
+      provide: SkyDocsToolsOptions,
+      useValue: {
+        gitRepoUrl: 'https://github.com/blackbaud/skyux-theme',
+        packageName: '@skyux/theme'
+      }
+    }
   ]
 })
 export class AppExtrasModule { }
