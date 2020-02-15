9ce0a77bfe5269f19a10bbb40a7afe2b0ca0f51d
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2020-02-15 16:05:36 -0500

    Angular 9; Drop RxJS 5

--- scripts/dev-bundle.js
+++ scripts/dev-bundle.js
@@ -1,8 +1,9 @@
 const fs = require('fs-extra');
-const path = require('path');
-const dest = './node_modules/@skyux/theme';
-
 const rimraf = require('rimraf');
+
+const json = fs.readJsonSync('./package.json');
+const dest = `./node_modules/${json.name}`;
+
 rimraf.sync(dest);
 
 fs.ensureDirSync(dest);
