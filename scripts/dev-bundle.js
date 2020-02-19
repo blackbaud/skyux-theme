b593831936c120a3b5f011965f273bd5380b71e8
Steve Brush <steve.brush@blackbaud.com>
2020-02-19 12:24:06 -0500

    [rc] Angular 9; drop RxJS 5 (#94)

--- scripts/dev-bundle.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs-extra');
-const path = require('path');
-const dest = './node_modules/@skyux/theme';
-
-const rimraf = require('rimraf');
-rimraf.sync(dest);
-
-fs.ensureDirSync(dest);
-fs.copySync('./dist', dest);
