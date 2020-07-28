4a318b3bf31c653bd659381ef283ad8eefd99f40
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2020-07-28 16:59:38 -0400

    Fix dev-bundle

--- scripts/dev-bundle.js
+++ scripts/dev-bundle.js
@@ -1,15 +1,25 @@
-const cli = require('@skyux-sdk/builder');
 const fs = require('fs-extra');
 const rimraf = require('rimraf');
+const crossSpawn = require('cross-spawn');
+
+function runAsync() {
+  const dest = './node_modules/@skyux/theme';
+  rimraf.sync(dest);
+
+  crossSpawn.sync('npx', [
+    '-p', '@skyux-sdk/cli',
+    'skyux', 'build-public-library',
+    '--logFormat', 'none'
+  ], {
+    stdio: 'inherit'
+  });
+
+  fs.ensureDirSync(dest);
+  fs.copySync('./dist', dest);
+}
 
 module.exports = {
-  runAsync: async () => {
-    const dest = './node_modules/@skyux/theme';
-    rimraf.sync(dest);
-
-    await cli.runCommand('build-public-library', {});
-
-    fs.ensureDirSync(dest);
-    fs.copySync('./dist', dest);
-  }
+  runAsync
 };
+
+runAsync();
