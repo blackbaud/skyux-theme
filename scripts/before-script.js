b20baa9acca578acfa310aafd26c853924bbdb97
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2020-07-28 17:22:48 -0400

    Fix script

--- scripts/before-script.js
+++ scripts/before-script.js
@@ -3,17 +3,21 @@ const crossSpawn = require('cross-spawn');
 const preparePackage = require('./prepare-package');
 const devBundle = require('./dev-bundle');
 
-module.exports = {
-  runAsync: () => {
-    crossSpawn.sync('npx', [
-      '-p', '@skyux-sdk/cli',
-      'skyux', 'build-public-library',
-      '--logFormat', 'none'
-    ], {
-      stdio: 'inherit'
-    });
+async function runAsync() {
+  crossSpawn.sync('npx', [
+    '-p', '@skyux-sdk/cli',
+    'skyux', 'build-public-library',
+    '--logFormat', 'none'
+  ], {
+    stdio: 'inherit'
+  });
 
-    await preparePackage.runAsync();
-    await devBundle.runAsync();
-  }
+  await preparePackage.runAsync();
+  await devBundle.runAsync();
+}
+
+module.exports = {
+  runAsync
 };
+
+runAsync();
