b6f25d8fb67f246090176c530ae8ecd16b2864a7
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2020-07-28 15:42:22 -0400

    Update dev-bundle.js

--- scripts/dev-bundle.js
+++ scripts/dev-bundle.js
@@ -1,9 +1,12 @@
+const cli = require('@skyux-sdk/builder');
 const fs = require('fs-extra');
-const path = require('path');
+
 const dest = './node_modules/@skyux/theme';
 
 const rimraf = require('rimraf');
 rimraf.sync(dest);
+
+cli.runCommand('build-public-library', {});
 
 fs.ensureDirSync(dest);
 fs.copySync('./dist', dest);
