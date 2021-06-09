7303c6ec1e944d5ab1b5dc6ea0ff7b7e0f04d989
Alex Kingman <alex.kingman@blackbaud.me>
2021-06-09 15:46:37 -0400

    added deprecation message

--- src/app/public/icons/icon-manifest.module.ts
+++ src/app/public/icons/icon-manifest.module.ts
@@ -2,5 +2,9 @@ import {
   NgModule
 } from '@angular/core';
 
+/**
+ * @deprecated `SkyThemeIconManifestModule` is no longer needed
+ * and can be removed from your providers.
+ */
 @NgModule()
 export class SkyThemeIconManifestModule { }
