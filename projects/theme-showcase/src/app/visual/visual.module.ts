5881f72e30a19f76bef5e6a307f2c9462fc6fded
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2021-09-09 14:00:04 -0400

    Next > Adopt Angular CLI

---
+++ projects/theme-showcase/src/app/visual/visual.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { VisualComponent } from './visual.component';
+
+
+
+@NgModule({
+  declarations: [
+    VisualComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class VisualModule { }
