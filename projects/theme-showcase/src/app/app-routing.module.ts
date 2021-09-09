6648a15a02cd2665b50edca857739bdbae19b7cc
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2021-09-09 15:35:42 -0400

    Routes

--- projects/theme-showcase/src/app/app-routing.module.ts
+++ projects/theme-showcase/src/app/app-routing.module.ts
@@ -1,7 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SkyBordersDemoComponent } from './visual/borders/borders-demo.component';
+import { SkyButtonsDemoComponent } from './visual/buttons/buttons-demo.component';
+import { HTMLInputsDemoComponent } from './visual/inputs/inputs-demo.component';
+import { SkyResponsiveDemoComponent } from './visual/responsive/responsive-demo.component';
+import { ThemingDemoComponent } from './visual/theming/theming-demo.component';
+import { SkyValidationDemoComponent } from './visual/validation/validation-demo.component';
+import { VisualComponent } from './visual/visual.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: VisualComponent
+  },
+  {
+    path: 'visual/borders',
+    component: SkyBordersDemoComponent
+  },
+  {
+    path: 'visual/buttons',
+    component: SkyButtonsDemoComponent
+  },
+  {
+    path: 'visual/inputs',
+    component: HTMLInputsDemoComponent
+  },
+  {
+    path: 'visual/responsive',
+    component: SkyResponsiveDemoComponent
+  },
+  {
+    path: 'visual/theming',
+    component: ThemingDemoComponent
+  },
+  {
+    path: 'visual/validation',
+    component: SkyValidationDemoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
