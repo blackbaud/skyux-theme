fdd12c13b0a57b45a823d0f4448f24839c9a525f
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2021-09-09 14:18:47 -0400

    Update build scripts

--- scripts/after-build-public-library-success.js
+++ scripts/after-build-public-library-success.js
@@ -1,6 +1,13 @@
+const crossSpawn = require('cross-spawn');
+
 async function runAsync() {
   console.log('Preparing package for NPM...');
-  require('./prepare-package');
+
+  crossSpawn.sync('node', ['./scripts/prepare-package.js'], {
+    cwd: process.cwd(),
+    stdio: 'inherit'
+  });
+
   console.log('Done preparing package.');
 }
 
--- scripts/dev-bundle.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs-extra');
-const dest = './node_modules/@skyux/theme';
-
-const rimraf = require('rimraf');
-rimraf.sync(dest);
-
-fs.ensureDirSync(dest);
-fs.copySync('./dist', dest);
